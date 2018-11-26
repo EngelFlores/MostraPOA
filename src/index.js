76a4ffb4307aaf799be3529a1255e462d98d7c89
EngelFlores <engel.tads@gmail.com>
2018-11-25 23:07:55 -0200

    Adding react router and creating first page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Index from './views/HomePage'
+import NotFound from './views/NotFound'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routes = () => (
+    <Fragment>
+      <Switch>
+        <Route path="/index" component={Index} />
+        <Redirect exact from="/" to="index" />
+        <Route component={NotFound} />
+      </Switch>
+    </Fragment>
+  );
+  
+  ReactDOM.render(
+    <BrowserRouter>{routes()}</BrowserRouter>,
+    document.getElementById('root')
+  );
+  
