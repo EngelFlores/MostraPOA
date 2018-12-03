4cbc837ad227f79801db33fdb35a848b8206d4d9
EngelFlores <engel.tads@gmail.com>
2018-12-03 11:24:28 -0200

    Adding hamburguer menu

--- src/components/Menu/dropDown.js
+++ src/components/Menu/dropDown.js
@@ -3,12 +3,124 @@ import { Link, Redirect } from 'react-router-dom';
 import { Tooltip } from 'react-tippy'
 import 'react-tippy/dist/tippy.css'
 import './dropDown.css'
+import './hamburguer.css'
 
 
 export const Menu = () => (
-    <div className='menu'>
+    <Fragment>
 
 
+        <nav className="header_mobile" role="navigation">
+            <div id="menutoggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul id="menu">
+                    <Tooltip
+                        position="bottom-start"
+                        html={
+                            <div className='container'>
+                                <Link to={'/'}>NOVIDADES</Link>
+                                <Link to={'/'}>REGULAMENTOS</Link>
+                                <Link to={'/'}>EVENTOS</Link>
+                            </div>
+                        }
+                        trigger="click"
+                        interactive="true"
+                        useContext="true"
+                        theme="light"
+                    >
+                        <p>INÍCIO</p>
+                    </Tooltip>
+
+                    <Tooltip
+                        position="bottom-start"
+                        html={
+                            <div className='container'>
+                                <Link to={'/'}>REGULAMENTO</Link>
+                                <Link to={'/'}>INSCRIÇÕES</Link>
+                                <Link to={'/'}>CRONOGRAMA</Link>
+                                <Link to={'/'}>PROGRAMAÇÃO</Link>
+                                <Link to={'/'}>HORÁRIO</Link>
+                                <Link to={'/'}>PALESTRANTES</Link>
+                            </div>
+                        }
+                        trigger="click"
+                        interactive="true"
+                        useContext="true"
+                        theme="light"
+                    >
+                        <p>MOSTRAPOA</p>
+                    </Tooltip>
+
+
+                    <Tooltip
+                        position="bottom-start"
+                        html={
+                            <div className='container'>
+                                <Link to={'/'}>REGULAMENTO</Link>
+                                <Link to={'/'}>INSCRIÇÕES</Link>
+                                <Link to={'/'}>CRONOGRAMA</Link>
+                                <Link to={'/'}>PROGRAMAÇÃO</Link>
+                                <Link to={'/'}>HORÁRIO</Link>
+                                <Link to={'/'}>PALESTRANTES</Link>
+                            </div>
+                        }
+                        trigger="click"
+                        interactive="true"
+                        useContext="true"
+                        theme="light"
+                    >
+                        <p>STARTPOA</p>
+                    </Tooltip>
+
+                    <Tooltip
+                        position="bottom-start"
+                        html={
+                            <div className='container'>
+                                <Link to={'/'}>REGULAMENTO</Link>
+                                <Link to={'/'}>INSCRIÇÕES</Link>
+                                <Link to={'/'}>CRONOGRAMA</Link>
+                                <Link to={'/'}>PROGRAMAÇÃO</Link>
+                                <Link to={'/'}>HORÁRIO</Link>
+                                <Link to={'/'}>PALESTRANTES</Link>
+                            </div>
+                        }
+                        trigger="click"
+                        interactive="true"
+                        useContext="true"
+                        theme="light"
+                    >
+                        <p>ROBÓTICA</p>
+                    </Tooltip>
+
+
+                    <Tooltip
+                        position="bottom-start"
+                        html={
+                            <div className='container'>
+                                <Link to={'/'}>SUBMISSÃO</Link>
+                                <Link to={'/'}>ACEITOS</Link>
+                                <Link to={'/'}>PREMIADOS</Link>
+                            </div>
+                        }
+                        trigger="click"
+                        interactive="true"
+                        useContext="true"
+                        theme="light"
+                    >
+                        <p>TRABALHOS</p>
+                    </Tooltip>
+
+                    <p onClick={<Redirect to={''}></Redirect>}>SOBRE</p>
+
+                    <p onClick={<Redirect to={''}></Redirect>}>CONTATO</p>
+                </ul>
+            </div>
+        </nav>
+
+        <div className='menu'>
             <Tooltip
                 position="bottom-start"
                 html={
@@ -105,34 +217,9 @@ export const Menu = () => (
                 <p>TRABALHOS</p>
             </Tooltip>
 
-            <Tooltip
-                position="bottom-start"
-                html={
-                    <div className='container'>
-                        <Redirect to={''}></Redirect>
-                    </div>
-                }
-                trigger="click"
-                interactive="true"
-                useContext="true"
-                theme="light"
-            >
-                <p>SOBRE</p>
-            </Tooltip>
+            <p onClick={<Redirect to={''}></Redirect>}>SOBRE</p>
 
-            <Tooltip
-                position="bottom-start"
-                html={
-                    <div className='container'>
-                        <Redirect to={''}></Redirect>
-                    </div>
-                }
-                trigger="click"
-                interactive="true"
-                useContext="true"
-                theme="light"
-            >
-                <p>CONTATO</p>
-            </Tooltip>
-    </div>
+            <p onClick={<Redirect to={''}></Redirect>}>CONTATO</p>
+        </div>
+    </Fragment>
 )
