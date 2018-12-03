d3ffa9609ef411207cc8bcd1ce9b4878a35830a4
jaquelinebonoto <39954954+jaquelinebonoto@users.noreply.github.com>
2018-12-03 17:39:41 -0200

    Pequenas alterações na Home

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -5,13 +5,22 @@ export default () => (
     <Fragment className="main">
         <div className="bgimg-1">
             <div className="caption">
-                <span className="border">FOTO 1</span>
+                <span className="border">MOSTRAPOA 2019</span>
             </div>
         </div>
 
-        <div style={{color:'FFFFF', backgroundcolor:'white', textalign:'center, justify'}}>
-            <h3 style={{textalign:'center' }}>Parallax Demo</h3>
-            <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+        <div className="row">
+            <div className="col">
+                <h1 style={{textAlign:'center'}}>19ª Mostra de Pesquisa, Ensino e Extensão</h1>
+            </div>
+            <div className="col">
+            <h4 style={{textAlign:'center'}}>Data do evento aqui</h4>
+            </div>
+            <div className="flex-column justify-content-center">
+                <a class="btn btn-primary btn-lg" href="#" role="button">NOVIDADES</a>
+                <a class="btn btn-primary btn-lg" href="#" role="button">REGULAMENTOS</a>
+                <a class="btn btn-primary btn-lg" href="#" role="button">EVENTOS</a>
+            </div>
         </div>
 
         <div className="bgimg-2">
@@ -40,7 +49,7 @@ export default () => (
 
         <div className="bgimg-1">
             <div className="caption">
-                <span className="border">COOL!</span>
+                <span className="border">INSCRIÇÕES EM BREVE.</span>
             </div>
         </div>
     </Fragment>
